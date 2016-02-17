53e1de30ada4fe5830d92b04e41a1b3a58083da8
bildin <aleksandr@bildin.ru>
2016-02-18 00:02:34 +0300

    beta 0.3.1
    added help toast on launch

--- src/eu/dedb/nfc/tagdiag/ServiceStarter.java
+++ src/eu/dedb/nfc/tagdiag/ServiceStarter.java
@@ -3,6 +3,8 @@ package eu.dedb.nfc.tagdiag;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Toast;
 
 public class ServiceStarter extends Activity {
 
@@ -11,6 +13,9 @@ public class ServiceStarter extends Activity {
 		super.onCreate(savedInstanceState);
 		Intent intent = new Intent(this, NfcDiagService.class);
 		startService(intent);
+		Toast msg = Toast.makeText(this, "Move TagDiag Navigation Icon to select action", Toast.LENGTH_LONG);
+		msg.setGravity(Gravity.CENTER, 0, 0);
+		msg.show();
 		finish();
 	}
 }
